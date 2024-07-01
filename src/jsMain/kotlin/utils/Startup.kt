1d6c6d70e9c7e6b1ec136e8a49c6cd0b021c7c47
ibm2187 <ibm.shaikhly@gmail.com>
2024-07-01 17:02:46 -0400

    fix paths

--- src/jsMain/kotlin/utils/Startup.kt
+++ src/jsMain/kotlin/utils/Startup.kt
@@ -1,14 +1,17 @@
 package utils
 
-
+/**
+ * We have to be careful about file movements or directory changes as they can break project initialization
+ * todo look for a better way to copy js project files
+ */
 class Startup {
 
     private val fs = require("fs")
 
     fun copyProjectFiles(errorCallback: (String?) -> Unit) {
         fs.copyFile(
-            "/Users/ibm2187/Desktop/kotlin-js/kotlin-js-test/index.js",
-            "/Users/ibm2187/Desktop/kotlin-js/kotlin-js-test/build/js/packages/ibmTest/kotlin/index.js"
+            "/Users/ibm2187/Desktop/kotlin-js/index.js",
+            "/Users/ibm2187/Desktop/kotlin-js/build/js/packages/ibmTest/kotlin/index.js"
         ) { error ->
             errorCallback(error?.message as? String)
         }
